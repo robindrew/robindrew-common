3cb9555ab93b420f70488ab8b47430f5421f900e
Robin Drew <robin.drew@gmail.com>
2020-04-21 14:51:28 +0100

    Formatting JSON

--- src/main/java/com/robindrew/common/json/Gsons.java
+++ src/main/java/com/robindrew/common/json/Gsons.java
@@ -1,11 +1,16 @@
 package com.robindrew.common.json;
 
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class Gsons {
 
+	public static String prettyPrint(Object object) {
+		return new GsonBuilder().setPrettyPrinting().create().toJson(object);
+	}
+	
 	public static IJson toJson(JsonObject object) {
 		return new JsonObjectWrapper(object);
 	}
